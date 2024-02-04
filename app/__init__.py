b2706f857fbf0aeda418a7f41e4ece66c7cfc408
Melker Widén <melker.widen@gmail.com>
2024-02-04 22:28:08 +0100

    Add contact and about pages. Improve homepage

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,16 @@ from flask import Flask
 
 from .config import CONFIGURATIONS
 from .views.home import home
+from .views.about import about
+from .views.contact import contact
 
 
 def create_app(configuration: str = "production") -> Flask:
     app = Flask(__name__)
     app.config.from_object(CONFIGURATIONS[configuration])
 
-    app.register_blueprint(home, url_prefix="")
+    app.register_blueprint(home, url_prefix="/")
+    app.register_blueprint(about, url_prefix="/about/")
+    app.register_blueprint(contact, url_prefix="/contact/")
 
     return app
---
+++ app/views/contact.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, render_template
+
+contact = Blueprint("contact", __name__)
+
+
+@contact.route("/")
+def index():
+    return render_template("contact/index.html")
