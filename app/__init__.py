7d8de2fb27f5271d616ffeac53dbef26d0f89881
Melker Widén <melker.widen@gmail.com>
2024-02-18 19:52:20 +0100

    Configure extensions

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,9 @@ from .views.home import home
 from .views.about import about
 from .views.contact import contact
 
+from .extensions.db import db
+from .extensions.mail import mail
+
 
 def create_app(configuration: str = "production") -> Flask:
     app = Flask(__name__)
@@ -13,5 +16,11 @@ def create_app(configuration: str = "production") -> Flask:
     app.register_blueprint(home, url_prefix="/")
     app.register_blueprint(about, url_prefix="/about/")
     app.register_blueprint(contact, url_prefix="/contact/")
+
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+
+    mail.init_app(app)
 
     return app
--- app/config.py
+++ app/config.py
@@ -8,18 +8,25 @@ load_dotenv()
 class BaseConfiguration:
     SECRET_KEY = getenv("SECRET_KEY")
 
+    SQLALCHEMY_DATABASE_URI = "sqlite:///app.db"
+
+    MAIL_SERVER = "smtp.gmail.com"
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = getenv("MAIL_USERNAME")
+    MAIL_PASSWORD = getenv("MAIL_APP_PASSWORD")
+    MAIL_DEFAULT_SENDER = "noreply@mewi.dev"
+
 
 class DevelopmentConfiguration(BaseConfiguration):
     DEBUG = True
 
 
 class ProductionConfiguration(BaseConfiguration):
-    DEBUG = False
     JSON_SORT_KEYS = False
 
 
 class TestingConfiguration(BaseConfiguration):
-    DEBUG = False
     TESTING = True
 
 
